6db849e0661da162b008289f20fe488d078483c3
Denis Volk <denis.volk@gmail.com>
2016-05-03 19:24:40 +0200

    update

--- bloggen.py
+++ bloggen.py
@@ -2,11 +2,11 @@
 
 from __future__ import unicode_literals, print_function
 import glob, codecs, os.path, math, datetime, argparse, re, types
-from os import mkdir
+from os import mkdir, chdir
 from shutil import rmtree, copyfile
 from bs4 import BeautifulSoup
 
-args = None
+# configuration
 site_name = 'dvolk.github.io'
 site_url = 'https://dvolk.github.io'
 author = 'Denis Volk'
@@ -21,8 +21,13 @@ top_links = [
     ("Github", "#97FFFF", "https://github.com/dvolk"),
     ("Flickr", "#FF6EB4", "https://www.flickr.com/photos/denis_volk")
 ]
+# end of configuration
+# see also the list "special_pages" below
 
-header = """<!DOCTYPE html>
+def header(title, author, description, site_root, levels):
+    if levels == 0: path = "."
+    else: path = ".."
+    return """<!DOCTYPE html>
 <html>
 <head>
 <title>{0}</title>
@@ -30,47 +35,31 @@ header = """<!DOCTYPE html>
 <meta name="generator" content="bloggen.py see https://github.com/dvolk/bloggen" />
 <meta name="author" content="{1}" />
 <meta name="viewport" content="width=device-width" />
-"""
-
-index_header = """<meta name="description" content="{2}" />
-<link rel="canonical" href="{0}" />
-<link rel="icon" href="./favicon.png" type="image/png" />
-<link rel="stylesheet" type="text/css" href="./index.css" />
-<link rel="alternate" type="application/rss+xml" href="./feed.rss" title="RSS feed for {1}">
+<meta name="description" content="{2}" />
+<link rel="icon" href="{5}/favicon.png" type="image/png" />
+<link rel="stylesheet" type="text/css" href="{5}/index.css" />
+<link rel="alternate" type="application/rss+xml" href="{5}/feed.rss" title="RSS feed for {4}">
 </head>
 <body>
 <div class='outer'>
 <div class='inner'>
 <div class='header'>
 <div class='logo'>
-<h1><a style='color: black;' href='./index.html'>{1}</a></h1>
+<h1><a style='color: black;' href='{5}'>{6}</a></h1>
 </div>
-<div class='navigation'>"""
+<div class='navigation'>""".format(title, author, description, site_root, site_name, path, site_name)
 
-post_header = """<meta name="description" content="{1}" />
-<link rel="icon" href="../favicon.png" type="image/png" />
-<link rel="stylesheet" type="text/css" href="../index.css" />
-<link rel="alternate" type="application/rss+xml" href="../feed.rss" title="RSS feed for {0}">
-</head>
-<body>
-<div class='outer'>
-<div class='inner'>
-<div class='header'>
-<div class='logo'>
-<h1><a style='color: black;' href='../index.html'>{0}</a></h1>
-</div>
-<div class='navigation'>"""
-
+# <div style="text-align: center"><p style="font-size: 0.8em; color: #777;">bloggen.py<a></p></div>
 footer = """</div>
 </div>
 </body>
 </html>"""
 
 class Post:
-    def __init__(self, link, snip, time, author, title, rest, want_toc):
+    def __init__(self, link, soup=None, snip=None, time=None, author=None, title=None, rest=None, want_toc=False):
         self.link, self.snip, self.time = link, snip, time
         self.author, self.title, self.rest = author, title, rest
-        self.want_toc = want_toc
+        self.want_toc, self.soup = want_toc, soup
 
 def mk_index_filename(page):
     if page == 0: return "index.html"
@@ -110,11 +99,15 @@ def process_vars(soup):
     for a in soup.find_all('a'):
         a['href'] = re.sub("__SITEROOT__", site_url, a['href'])
         a['href'] = re.sub("__SITEPOST__", posts, a['href'])
+    for img in soup.find_all('img'):
+        img['src'] = re.sub("__SITEROOT__", site_url, img['src'])
+        img['src'] = re.sub("__SITEPOST__", posts, img['src'])
     return soup
 
 def mk_entry(post, in_entry):
     ret = '<div class="entry">\n'
-    path = site_url + "/posts"
+    if in_entry: path = "."
+    else: path = "./posts"
     ret += "<h2 style='display: inline'><a href='{0}/{1}.html'>{2}</a></h2>\n".format(path, post.link, post.title)
     if post.author or post.time:
         ret += "<br/><p style='font-size: 0.8em; display: inline'>"
@@ -159,16 +152,17 @@ def make_rss(posts, how_many):
     with codecs.open('./generated/feed.rss', mode='w', encoding='utf-8') as rss_file:
         rss.write_xml(rss_file)
 
-def nav_links(out, in_entry):
+def nav_links(in_entry):
     # this span maintains the layout if there are no top links or rss
-    out.write("<p><span style='font-size: 1.2em; visibility: hidden'>.</span>\n")
+    out = "<p><span style='font-size: 1.2em; visibility: hidden'>.</span>\n"
     for link in top_links:
         back = "." if in_entry else "./posts"
-        out.write("<a style='background-color: {0}' class='top-link' href='{1}'>{2}</a> \n".format(link[1], link[2].format(back), link[0]))
+        out += "<a style='background-color: {0}' class='top-link' href='{1}'>{2}</a> \n".format(link[1], link[2].format(back), link[0])
     if create_rss:
-        if in_entry: out.write("<a id='rss' href='../feed.rss'>RSS</a>")
-        else: out.write("<a id='rss' href='./feed.rss'>RSS</a>")
-    out.write("</p>\n</div>\n</div>\n")
+        back = ".." if in_entry else "."
+        out += "<a id='rss' href='{0}/feed.rss'>RSS</a>".format(back)
+    out += "</p>\n</div>\n</div>\n"
+    return out
 
 def parse_post(contents, in_filename):
     # parse the input post file
@@ -185,22 +179,22 @@ def parse_post(contents, in_filename):
     if title_content is not None: title_content = title_content.text
     else: title_content = os.path.basename(in_filename)
     want_toc = True if want_toc is not None else False
-    post = Post(os.path.basename(in_filename),
+    post = Post(os.path.basename(in_filename), soup,
                 snip_content, time_content, author_content, title_content, rest_content, want_toc)
     return post
 
-def write_post(post, in_filename):
+def write_post(post):
     # write blog post file
-    out_filename = './generated/posts/{0}.html'.format(os.path.basename(in_filename))
+    out_filename = './generated/posts/{0}.html'.format(post.link)
     with codecs.open(out_filename, mode = 'w', encoding="utf-8", errors="xmlcharrefreplace") as out_doc:
-        out_doc.write(header.format("{0} - {1}".format(post.title, site_name), author))
         # text to put into the description meta tag
         description = post.snip.get_text() if post.snip else ""
         description = description.replace('"', '')
         description = description.replace('\n', ' ')
         description = description.strip()
-        out_doc.write(post_header.format(site_name, description))
-        nav_links(out_doc, in_entry = True)
+        title = "{0} - {1}".format(post.title, site_name)
+        out_doc.write(header(title, author, description, site_url, 1))
+        out_doc.write(nav_links(in_entry = True))
         out_doc.write(mk_entry(post, in_entry = True))
         out_doc.write(footer)
     return post
@@ -209,34 +203,34 @@ posts = []
 
 def all_posts_by_date():
     global posts
-    rest = ""
+    rest = "<ul>\n"
     for post in posts:
-        rest += "<p><a href='{0}.html'>{1}</a>".format(post.link, post.title)
+        rest += "<li><a href='{0}.html'>{1}</a>".format(post.link, post.title)
         if post.time:
             time_str = datetime.datetime.strftime(post.time, post_time_output_fmt)
             rest += " <span class='date_text'>{0}</span>".format(time_str)
-        rest += "</p>\n"
-    return Post("all_posts_by_date", None, None, None,
-                "All posts by date", rest, False)
+        rest += "</li>\n"
+    rest += "</ul>\n"
+    return Post(link="all_posts_by_date", title="All posts by date", rest=rest)
+
+def list_all_posts_about(name):
+    rest = "<ul>\n"
+    for post in posts:
+        if not re.search(name, post.title): continue
+        rest += "<li><a href='{0}.html'>{1}</a>".format(post.link, post.title)
+        if post.time:
+            rest += " <span class='date_text'>{0}</span>".format(datetime.datetime.strftime(post.time, post_time_output_fmt))
+        rest += "</li>\n"
+    rest += "</ul>\n"
+    return rest
 
 def all_posts_about(*fun_args):
     name = fun_args[0]
     tag = fun_args[1]
     global posts
-    rest = "<p>Auto-generated list of all posts containing \"{0}\" in the title:<p>".format(name)
-    for post in posts:
-        if not re.search(name, post.title): continue
-        rest += "<p><a href='{0}.html'>{1}</a>".format(post.link, post.title)
-        if post.time:
-            rest += " <span class='date_text'>{0}</span>".format(datetime.datetime.strftime(post.time, post_time_output_fmt))
-        rest += "</p>\n"
-    return Post("all_posts_about_{0}".format(tag), None, None, None, "All posts about {0}".format(name), rest, False)
-
-def index_special_pages(s_posts):
-    rest = "<p>This page contains a list of all auto-generated pages:</p>"
-    for post in s_posts:
-        rest += "<p><a href='{0}.html'>{1}</a></p>".format(post.link, post.title)
-    return Post("autogenned_page_index", None, None, None, "Auto-generated pages", rest, False)
+    rest = "<p>List of all posts containing \"{0}\" in the title:<p>\n".format(name)
+    rest += list_all_posts_about(name)
+    return Post(link="all_posts_about_{0}".format(tag), title="All posts about {0}".format(name), rest=rest)
 
 # to generate posts from functions rather than files,
 # add them to special_pages. The elements of special pages are either
@@ -246,18 +240,52 @@ special_pages = [all_posts_by_date,
                  (all_posts_about, ('Project X', 'project_x')),
                  (all_posts_about, ('bloggen.py', 'bloggen_py'))]
 
+def index_special_pages(s_posts):
+    rest = "<p>List of all auto-generated pages:</p>\n<ul>\n"
+    for post in s_posts:
+        rest += "<li><a href='{0}.html'>{1}</a></li>".format(post.link, post.title)
+    rest += "</ul>\n"
+    return Post(link="autogenned_page_index", title="Auto-generated pages", rest=rest)
+
+def postprocess(into_post):
+    """Modify post soup after all the posts are loaded"""
+    soup = into_post.soup
+    if not soup: return
+    # insert list of posts matching into span of class list_all_posts_containing
+    for searched in soup.find_all("span", {"class": "list_all_posts_containing"}):
+        name = searched.get_text()
+        searched.clear()
+        ul = soup.new_tag("ul")
+        for post in posts:
+            if post is into_post or not re.search(name, post.title):
+                continue
+            li = soup.new_tag("li")
+            a = soup.new_tag("a", href="./{0}.html".format(post.link))
+            a.append(post.title + ' ')
+            li.append(a)
+            if post.time:
+                time_str = datetime.datetime.strftime(post.time, post_time_output_fmt)
+                datetext = soup.new_tag("span", **{'class':'date_text'})
+                datetext.append(time_str)
+                li.append(datetext)
+            ul.append(li)
+            searched.append(ul)
+
 def main():
     ap = argparse.ArgumentParser()
-    ap.add_argument('-g', '--generate',   type = str, nargs = 1)
-    ap.add_argument('-n', '--no-index',   action = "store_true", help = "don't generate index")
-    ap.add_argument('-s', '--skip-files', action = "store_true", help = "skip files beginning with . # or -")
-    ap.add_argument('-q', '--quiet',      action = "store_true")
-    ap.add_argument('-r', '--root',       type = str, nargs = 1, help = "override site root path")
+    ap.add_argument('-g', '--generate',    type = str, nargs = 1)
+    ap.add_argument('-n', '--no-index',    action = "store_true", help = "don't generate index")
+    ap.add_argument('-s', '--skip-files',  action = "store_true", help = "skip files beginning with . # or -")
+    ap.add_argument('-q', '--quiet',       action = "store_true")
+    ap.add_argument('-r', '--root',        type = str, nargs = 1, help = "override site root path")
+    ap.add_argument('-t', '--test-server', action = "store_true", help = "start SimpleHTTPServer on localhost:9999")
     args = ap.parse_args()
 
     if args.root is not None:
         global site_url
         site_url = args.root[0]
+#    if args.test_server is not None:
+#        site_url = "http://127.0.0.1:9999"
 
     if not args.generate and not args.no_index:
         if os.path.exists('./generated/'):
@@ -285,28 +313,30 @@ def main():
         with codecs.open(in_filename, mode="r", encoding="utf-8") as doc:
             post = parse_post(doc.read(), in_filename)
             posts.append(post)
-            write_post(post, in_filename)
 
     # sort posts by date with posts with no date at the bottom
     posts = sorted(posts, key=lambda x: (x.time is not None, x.time), reverse=True)
 
-    pages = int(math.ceil(float(len(posts)) / posts_per_page))
+    for post in posts:
+        postprocess(post)
+        write_post(post)
 
     special_posts = []
     for special_page in special_pages:
         if type(special_page) == types.FunctionType:
             post = special_page()
-            write_post(post, special_page.__name__)
+            post.link = special_page.__name__
+            write_post(post)
         if type(special_page) == tuple:
             fun = special_page[0]
             assert type(fun) == types.FunctionType
             post = fun(*(special_page[1]))
-            write_post(post, post.link)
+            write_post(post)
         posts.append(post)
         special_posts.append(post)
 
     autogen_index_post = index_special_pages(special_posts)
-    write_post(autogen_index_post, autogen_index_post.link)
+    write_post(autogen_index_post)
     posts.append(autogen_index_post)
 
     if args.no_index:
@@ -314,12 +344,13 @@ def main():
 
     if create_rss: make_rss(posts, posts_per_page)
 
+    pages = int(math.ceil(float(len(posts)) / posts_per_page))
+
     # generate index pages
     for page in range(pages):
         with codecs.open('./generated/' + mk_index_filename(page), mode='w', encoding="utf-8", errors="xmlcharrefreplace") as index:
-            index.write(header.format(site_name, author))
-            index.write(index_header.format(site_url, site_name, index_description))
-            nav_links(index, in_entry = False)
+            index.write(header(site_name, author, index_description, site_url, 0))
+            index.write(nav_links(in_entry = False))
 
             for post in posts[(page * posts_per_page):((page + 1) * posts_per_page)]:
                 if not args.quiet:
@@ -328,5 +359,18 @@ def main():
 
             mk_pagination(index, page, pages)
             index.write(footer)
+
+    if args.test_server:
+        chdir('./generated')
+        try:
+            import SimpleHTTPServer as httpserver
+            import SocketServer as socketserver
+        except ImportError: # py3
+            import http.server as httpserver
+            import socketserver
+        Handler = httpserver.SimpleHTTPRequestHandler
+        httpd = socketserver.TCPServer(("127.0.0.1", 9999), Handler)
+        print("serving at http://127.0.0.1:9999")
+        httpd.serve_forever()
 
 main()
